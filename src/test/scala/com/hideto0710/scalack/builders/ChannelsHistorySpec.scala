268baf96732c5d645c4bb0118602394622a3d8cb
hideto0710 <h.inamura0710@gmail.com>
2015-10-30 11:11:19 +0900

    テスト方法変更
    トークンがない場合でのテストコード作成

--- src/test/scala/com/hideto0710/scalack/builders/ChannelsHistorySpec.scala
+++ src/test/scala/com/hideto0710/scalack/builders/ChannelsHistorySpec.scala
@@ -13,48 +13,32 @@ class ChannelsHistorySpec extends FlatSpec() with Matchers {
   val conf = ConfigFactory.load()
   implicit val auth = Auth(conf.getString("develop.token"))
 
-  val channelList = Scalack.listChannels.excludeArchived(1).syncExecute match {
-    case Right(r) => Some(r.channels.getOrElse(Seq.empty))
-    case Left(e) => None
-  }
-  val channel = channelList.get.head.id
-
-  "An ChannelsHistory" should "be able to get channel history" in {
-    val f = Scalack.channelsHistory.channel(channel).count(1).execute
-    val result = Await.result(f, Duration.Inf)
-    result.ok should be (right = true)
-    result.messages.toArray should not be empty
+  "An ChannelsHistory" should "be able to get channel" in {
+    val s = Scalack.channelsHistory.channel("general").count(1)
+    s._channel.get should be ("general")
   }
 
-  it should "be able to get channel history sync" in {
-    Scalack.channelsHistory.channel(channel).count(1).syncExecute match {
-      case Right(r) =>
-        r.ok should be (right = true)
-        r.messages.get should not be empty
-      case Left(e) => println(e)
-    }
+  it should "be able to get count" in {
+    val s = Scalack.channelsHistory.channel("general").count(1)
+    s._count.get should be (1)
   }
 
   it should "be not able to get channel history because of toke" in {
-    val f = Scalack.channelsHistory.channel(channel).count(1).execute(Auth("v"))
+    val f = Scalack.channelsHistory.channel("general").count(1).execute(Auth("v"))
     val result = Await.result(f, Duration.Inf)
     result.ok should be (right = false)
     result.error.get should be ("invalid_auth")
   }
 
   // MARK: To check builder is immutable
-  val builder = Scalack.channelsHistory.channel(channel)
+  val builder = Scalack.channelsHistory
   it should "be able to get 2 messages" in {
-    val f = builder.count(2).execute
-    val result = Await.result(f, Duration.Inf)
-    result.ok should be (right = true)
-    result.messages.get.length should equal(2)
+    val s = builder.count(2)
+    s._count.get should be (2)
   }
 
   it should "be able to get 3 messages" in {
-    val f = builder.count(3).execute
-    val result = Await.result(f, Duration.Inf)
-    result.ok should be (right = true)
-    result.messages.get.length should equal(3)
+    val s = builder.count(3)
+    s._count.get should be (3)
   }
 }
--- src/test/scala/com/hideto0710/scalack/builders/ListChannelsSpec.scala
+++ src/test/scala/com/hideto0710/scalack/builders/ListChannelsSpec.scala
@@ -13,20 +13,9 @@ class ListChannelsSpec extends FlatSpec() with Matchers {
   val conf = ConfigFactory.load()
   implicit val auth = Auth(conf.getString("develop.token"))
 
-  "An SlackApiClient" should "be able to get channel list" in {
-    val f = Scalack.listChannels.excludeArchived(1).execute
-    val result = Await.result(f, Duration.Inf)
-    result.ok should be (right = true)
-    result.channels.toArray should not be empty
-  }
-
-  it should "be able to get channel list sync" in {
-    Scalack.listChannels.excludeArchived(0).syncExecute match {
-      case Right(r) =>
-        r.ok should be (right = true)
-        r.channels.get should not be empty
-      case Left(e) => println(e)
-    }
+  "An SlackApiClient" should "be able to get excludeArchived" in {
+    val s = Scalack.listChannels.excludeArchived(1)
+    s._excludeArchived.getOrElse(0) should be (1)
   }
 
   it should "be not able to get channel list because of toke" in {
